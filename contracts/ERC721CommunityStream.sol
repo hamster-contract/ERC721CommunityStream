5dc37b7b3d975ca04e775d45048487b4ecac3728
mohaijiang <mohaijiang110@163.com>
2023-01-06 15:28:18 +0800

    init

---
+++ contracts/ERC721CommunityStream.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract MyNFT is Initializable, ERC721Upgradeable, OwnableUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC721_init("MyNFT", "MT");
+        __Ownable_init();
+    }
+}
