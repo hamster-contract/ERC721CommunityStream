fea26771d5a053f6d11884b73c1674599b0d2f63
abing <410174833@qq.com>
2023-02-24 17:37:58 +0800

    unit test

--- contracts/ERC721CommunityStream.sol
+++ contracts/ERC721CommunityStream.sol
@@ -2,69 +2,8 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
 
-contract MyERC721Token is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable, ERC721Burnable, EIP712, ERC721Votes {
-    constructor() ERC721("MyERC721Token", "MTK") EIP712("MyERC721Token", "1") {}
-
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
-    function safeMint(address to, uint256 tokenId, string memory uri)
-    public
-    onlyOwner
-    {
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-    internal
-    whenNotPaused
-    override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId, batchSize);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _afterTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-    internal
-    override(ERC721, ERC721Votes)
-    {
-        super._afterTokenTransfer(from, to, tokenId, batchSize);
-    }
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-    public
-    view
-    override(ERC721, ERC721URIStorage)
-    returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-    public
-    view
-    override(ERC721, ERC721Enumerable)
-    returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+contract ERC721CommunityStream is ERC721, Ownable {
+    constructor() ERC721("ERC721CommunityStream", "MTK") {}
 }
--- test/ERC721CommunityStream.test.js
+++ test/ERC721CommunityStream.test.js
@@ -1,63 +1,14 @@
-const MyERC721Token = artifacts.require("MyERC721Token");
+const ERC721CommunityStream = artifacts.require("ERC721CommunityStream");
 
-contract("MyERC721Token", (accounts) => {
-    let token;
+contract("ERC721CommunityStream", (accounts) => {
+    let contractInstance;
 
     beforeEach(async () => {
-        token = await MyERC721Token.new({ from: accounts[0] });
+        contractInstance = await ERC721CommunityStream.new({ from: accounts[0] });
     });
 
-    it("should have the correct name and symbol", async () => {
-        const name = await token.name();
-        const symbol = await token.symbol();
-
-        assert.equal(name, "MyERC721Token");
-        assert.equal(symbol, "MTK");
+    it("should deploy the contract properly", async () => {
+        assert.equal(await contractInstance.name(), "ERC721CommunityStream");
+        assert.equal(await contractInstance.symbol(), "MTK");
     });
-
-    it("should allow the owner to pause and unpause the contract", async () => {
-        const isPaused = await token.paused();
-
-        assert.equal(isPaused, false);
-
-        await token.pause({ from: accounts[0] });
-        const isPausedAfterPause = await token.paused();
-        assert.equal(isPausedAfterPause, true);
-
-        await token.unpause({ from: accounts[0] });
-        const isPausedAfterUnpause = await token.paused();
-        assert.equal(isPausedAfterUnpause, false);
-    });
-
-    it("should allow the owner to mint a new token", async () => {
-        const uri = "https://example.com/token.json";
-        const tokenId = 1;
-
-        await token.safeMint(accounts[0], tokenId, uri, { from: accounts[0] });
-
-        const owner = await token.ownerOf(tokenId);
-        const tokenUri = await token.tokenURI(tokenId);
-
-        assert.equal(owner, accounts[0]);
-        assert.equal(tokenUri, uri);
-    });
-
-    it("should allow transfer of tokens", async () => {
-        const uri = "https://example.com/token.json";
-        const tokenId = 1;
-
-        await token.safeMint(accounts[0], tokenId, uri, { from: accounts[0] });
-
-        const ownerBefore = await token.ownerOf(tokenId);
-
-        await token.safeTransferFrom(accounts[0], accounts[1], tokenId, {
-            from: accounts[0],
-        });
-
-        const ownerAfter = await token.ownerOf(tokenId);
-
-        assert.equal(ownerBefore, accounts[0]);
-        assert.equal(ownerAfter, accounts[1]);
-    });
-
 });
